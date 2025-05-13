3e6fa343e2df0131210d2752a133a1d31dd2f573
tech-girl-15 <shraddhathorbole15@gmail.com>
2025-05-13 22:26:29 +0530

    Add files via upload

---
+++ sd github action/README.md
@@ -0,0 +1,18 @@
+# exp22
+
+
+change github -> .github
+
+
+
+git init
+
+git add .
+
+git commit -m "Add GitHub Actions workflow"
+
+git branch -M main
+
+git remote add origin https://github.com/<USERNAME>/exp22.git
+
+git push -u origin main
---
+++ sd github action/exp22/scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Starting build process..."
+echo "Compiling code..."
+sleep 1
+echo "Build complete at $(date)"
+echo "Rebuilding project - $(date)"
+
+
+
+git init
+git add .
+git commit -m "Add GitHub Actions workflow"
+git branch -M main
+git remote add origin git https://github.com/Sarthakbelvalkar23/devops_6th_prac.git
+git push -u origin main
